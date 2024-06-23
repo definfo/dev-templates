84ce262549fd5f3aba592e46b8ec455556e54b1c
definfo <hjsdbb1@gmail.com>
2024-06-23 13:39:42 +0800

    add: OCaml and Coq entries

--- flake.nix
+++ flake.nix
@@ -191,6 +191,16 @@
           description = "Node.js development environment";
         };
 
+        ocaml = {
+          path = ./ocaml;
+          description = "Ocaml development environment";
+        };
+        
+        coq = {
+          path = ./coq-opam;
+          description = "CoqCoq development environment";
+        };
+
         opa = {
           path = ./opa;
           description = "Open Policy Agent development environment";
